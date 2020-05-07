3784910180a57d9f700a5641fe73481d841c0239
Jordan Hayes <hayesja99@gmail.com>
2020-05-06 22:23:41 -0400

    create with alert controller

--- MovieQuotes/MovieQuotesTableViewController.swift
+++ MovieQuotes/MovieQuotesTableViewController.swift
@@ -16,6 +16,34 @@ class MovieQuotesTableViewController: UITableViewController {
         super.viewDidLoad()
         movieQuotes.append(MovieQuite(quote: "I'll be back", movie: "Terminator"))
         movieQuotes.append(MovieQuite(quote: "Yo Adrian!", movie: "Rocky"))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showAddQuoteDialog))
+    }
+    
+   @objc func showAddQuoteDialog() {
+        let alertController = UIAlertController(title: "Create a new movie quote", message: "", preferredStyle: .alert)
+    
+        alertController.addTextField { (textfield) in
+            textfield.placeholder = "Quote"
+        }
+        
+        alertController.addTextField { (textfield) in
+            textfield.placeholder = "Movie"
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+    
+    let submitAction = UIAlertAction(title: "Create Quote", style: .default, handler: { (action) in let quoteTextFields = alertController.textFields![0] as UITextField
+        let movieTextFields = alertController.textFields![1] as UITextField
+        let newMoview = MovieQuite(quote: quoteTextFields.text!, movie: movieTextFields.text!)
+        self.movieQuotes.insert(newMoview, at: 0)
+        self.tableView.reloadData()
+        
+    })
+        alertController.addAction(submitAction)
+    
+        present(alertController, animated: true, completion: nil)
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
