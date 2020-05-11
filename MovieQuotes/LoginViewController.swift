4cf5964bea8fc81ff3acd4acce32251c6526bce6
Jordan Hayes <hayesja99@gmail.com>
2020-05-11 01:24:04 -0400

    UI for email password

---
+++ MovieQuotes/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  MovieQuotes
+//
+//  Created by CSSE Department on 5/11/20.
+//  Copyright © 2020 CSSE Department. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func pressedSignInNewUser(_ sender: Any) {
+    }
+  
+    @IBAction func pressedLoginExistingUser(_ sender: Any) {
+    }
+    var showListSegueID = "ShowListSegue"
+}
