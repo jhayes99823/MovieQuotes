3840adfbdb161b1e9e24b04802c3a6bc0bf38ba6
Jordan Hayes <hayesja99@gmail.com>
2020-05-11 01:36:54 -0400

    signing and login with email and password

--- MovieQuotes/LoginViewController.swift
+++ MovieQuotes/LoginViewController.swift
@@ -7,15 +7,49 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class LoginViewController: UIViewController {
+    var showListSegueID = "ShowListSegue"
+
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        emailTextField.placeholder = "Email"
+        passwordTextField.placeholder = "Password"
+    }
+    
     @IBAction func pressedSignInNewUser(_ sender: Any) {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        
+        Auth.auth().createUser(withEmail: email, password: password) { (authRes, error) in
+            if let error = error {
+                print ("error creatign a new user for email/password \(error)")
+                return
+            }
+            
+            print("new user created and now signed in")
+            self.performSegue(withIdentifier: self.showListSegueID, sender: self)
+        }
     }
   
     @IBAction func pressedLoginExistingUser(_ sender: Any) {
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (authRes, error) in
+            if let error = error {
+                print ("error creatign a new user for email/password \(error)")
+                return
+            }
+            
+            print("signed in existing user")
+            self.performSegue(withIdentifier: self.showListSegueID, sender: self)
+        }
     }
-    var showListSegueID = "ShowListSegue"
+    
 }
